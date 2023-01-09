59a5c7f73ca5c61d0e0339183995f45215551131
William Li <williamli@Williams-MacBook-Air-5.local>
2023-01-08 19:10:00 -0500

    added a visitor tracker and changed the link because I forgot before

--- src/homePage.js
+++ src/homePage.js
@@ -8,8 +8,8 @@ function HomePage(){
                  and click submit once done</div>
             <div className='homeDiv'>Any questions or problems? Contact me on Instagram <a className='link' rel="noopener noreferrer" target={"_blank"} href='https://www.instagram.com/w.liam.li/'>@w.liam.li</a></div>
             <div className='homeDiv'>Disclaimer! This website is not affiliated in any sort of way with John Abbott College</div>
-            <div className='homeDiv'>Love this website? Follow me <a className='link' rel="noopener noreferrer" target={"_blank"} href='https://www.instagram.com/w.liam.li/'>@jac.schedule</a> to support this project!</div>
-            <div className='homeDiv'>To add a review, message <a className='link' rel="noopener noreferrer" target={"_blank"} href='https://www.instagram.com/w.liam.li/'>@jac.schedule</a> to give a review. Any review, good or bad is welcome!</div>
+            <div className='homeDiv'>Love this website? Follow me <a className='link' rel="noopener noreferrer" target={"_blank"} href='https://www.instagram.com/jac.schedule/'>@jac.schedule</a> to support this project!</div>
+            <div className='homeDiv'>To add a review, message <a className='link' rel="noopener noreferrer" target={"_blank"} href='https://www.instagram.com/jac.schedule/'>@jac.schedule</a> to give a review. Any review, good or bad is welcome!</div>
         </div>
     )
 }
